902b65e4b99a66d2d7848000f94637e09d8566e4
Maxwell Wang <maxwellwang@nbp-159-85.nbp.ruw.rutgers.edu>
2023-05-01 18:50:56 -0400

    Added source code link to menu

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Menu() {
   return (
@@ -8,6 +9,11 @@ export default function Menu() {
       </li>
       <li>
         <Link href="/ecommerce">Ecommerce</Link>
+      </li>
+      <li>
+        <Link href="https://github.com/maxwellwang/portfolio" target="_blank">
+          Source Code
+        </Link>
       </li>
     </ul>
   );
